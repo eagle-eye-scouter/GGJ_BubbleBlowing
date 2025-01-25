dd78eb7c7fc13040a2cafe6b03305540a5a4591f
eagle-eye-scouter <dhopkephotos@gmail.com>
2025-01-25 14:56:40 -0500

    Spawner makes Obstacle move + new Bird

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -1,18 +1,40 @@
 extends Node2D
+class_name Obstacle
 
 var hurtbox : Area2D
+var starting_position : Vector2
+
+@export var velocity : float = 10
+@export var direction := Vector2.ONE
+var state := State.IDLE 
+enum State {
+	IDLE,
+	ACTIVE,
+	RESET
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hurtbox.area_entered.connect(_on_area_entered)
-	pass # Replace with function body.
+	starting_position = global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if state == State.ACTIVE:
+		global_position += direction.normalized() * velocity * delta
+		_active_process(delta)
+	elif state == State.RESET:
+		global_position = starting_position
+	
+	if abs(global_position.x) > 1000:
+		state = State.RESET
 
 
 func _on_area_entered(area:Node2D):
 	if area is GumBubble:
 		area.pop()
+
+
+func _active_process(delta: float) -> void:
+	pass
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -7,9 +7,13 @@ extends Marker2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	trigger.body_entered.connect(_on_body_entered)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_body_entered(body:Node2D):
+	if body is Player:
+		for child in fleet.get_children():
+			if child is Obstacle:
+				child.state = Obstacle.State.ACTIVE
 	pass
