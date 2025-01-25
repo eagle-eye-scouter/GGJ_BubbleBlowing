f4dcd3875b6e65d609190c55622598b820ee9c09
Andrew L <129340220+AndrewLukan@users.noreply.github.com>
2025-01-25 16:34:20 -0500

    Main Menu Buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready() -> void:
-	get_node("VBoxContainer/ButtonPlay").grab_focus()
+	get_node("ButtonPlay").grab_focus()
 
 func _on_button_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/scroll_world.tscn")
