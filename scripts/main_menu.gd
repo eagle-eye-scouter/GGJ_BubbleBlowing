19c26c50f05a4349c2b3c4f815f8ede68fe9b05f
Andrew L <129340220+AndrewLukan@users.noreply.github.com>
2025-01-25 15:43:15 -0500

    Created Main Menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready() -> void:
+	get_node("VBoxContainer/ButtonPlay").grab_focus()
+
+func _on_button_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/scroll_world.tscn")
+
+func _on_button_leaderboard_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/scoreboard.tscn")
+
+func _on_button_exit_pressed() -> void:
+	get_tree().quit()
