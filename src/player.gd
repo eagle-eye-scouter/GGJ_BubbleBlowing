a61dbed7dc72375a48e6679d16a67c3333ebcc74
eagle-eye-scouter <dhopkephotos@gmail.com>
2025-01-24 22:46:38 -0500

    Update player controls

--- src/player.gd
+++ src/player.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 const HORIZONTAL_SPEED : float = 1000.0
+const VERTICAL_SPEED : float = 900.0
+const INFLATE_SPEED : float = 90.0
+const DEFLATE_SPEED : float = 45.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,9 +13,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var vector : Vector2 = Input.get_vector("player_left", "player_right", "ui_up", "ui_down")
+	var vector : Vector2 = Input.get_vector("player_left", "player_right", "player_up", "player_down")
 	
-	self.position.x += delta * vector.x * HORIZONTAL_SPEED
+	self.position = vector * Vector2(HORIZONTAL_SPEED, VERTICAL_SPEED) * delta
 	#print("delta", delta, ";vector", vector)
+	
+	
 	
 	pass
